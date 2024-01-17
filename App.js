512be78747b568c826d80a0b7a77c6b5480d9ef5
Christy Segunto <83587757+ChristySegunto@users.noreply.github.com>
2024-01-18 03:36:40 +0800

    CHANGES: organized stack

--- App.js
+++ App.js
@@ -1,30 +1,91 @@
-import React from 'react';
+import React, { createContext, useContext, useEffect, useState } from 'react';
 import 'react-native-gesture-handler';
+
 import Login from './Screens/Login';
 import Register from './Screens/Register';
 import Home_Fireauth from './Screens/Home_Fireauth';
 import Home_Resident from './Screens/Home_Resident';
+import Chat1 from './Screens/Chat1';
+import Chat from './Screens/Chat';
+
+import { onAuthStateChanged } from 'firebase/auth';
+import { AuthProvider } from './Firebase/AuthContext';
+import { ActivityIndicator, View } from 'react-native';
+import { auth } from './Firebase/firebase'
+
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
 
+const Stack = createStackNavigator();
+const AuthenticatedUserContext = createContext({});
 
-const App: () => Node = () => {
-  const Stack = createStackNavigator();
+const AuthenticatedUserProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
 
+  return(
+    <AuthenticatedUserContext.Provider value={{user, setUser}}>
+      {children}
+    </AuthenticatedUserContext.Provider>
+  )
+}
+
+function ChatStack (){
+  return(
+    <Stack.Navigator defaultScreenOptions={Home_Resident}>
+      <Stack.Screen name="Home_Resident" component={Home_Resident} options={{ headerShown: false }} />
+      <Stack.Screen name="Home_Fireauth" component={Home_Fireauth} options={{ headerShown: false }} />
+      <Stack.Screen name="Chat" component={Chat} />
+    </Stack.Navigator>
+  )
+}
+
+function AuthStack() {
   return (
+    <Stack.Navigator defaultScreenOptions={Login}>
+      <Stack.Screen name="Login" component={Login} options={{ headerShown: false }} />
+      <Stack.Screen name="Register" component={Register} options={{ headerShown: false }} />
+    </Stack.Navigator>
+  )
+}
+
+function RootNavigator(){
+  const { user, setUser } = useContext(AuthenticatedUserContext);
+  const [isLoading, setLoading] = useState(true);
+  useEffect(()=> {
+    const unsubscribe = onAuthStateChanged(auth,
+      async authenticatedUser =>{
+        authenticatedUser ? setUser(authenticatedUser) : setUser(null);
+        setLoading(false);
+      }
+    )
+    return () => unsubscribe();
+  }, [user]);
+
+  if(isLoading){
+    return(
+      <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+        <ActivityIndicator size="large"/>
+      </View>
+    )
+  }
+
+  return(
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Login" component={Login} options={{ headerShown: false }} />
-        <Stack.Screen name="Register" component={Register} options={{ headerShown: false }} />
-        <Stack.Screen name="Home_Resident" component={Home_Resident} options={{ headerShown: false }} />  
-        <Stack.Screen name="Home_Fireauth" component={Home_Fireauth} options={{ headerShown: false }} />
-      </Stack.Navigator>
+      { user ? <ChatStack/> : <AuthStack/> }
     </NavigationContainer>
-  );
-};
+  )
+}
 
 
-export default App;
+export default function App(){
+  return(
+    <AuthenticatedUserProvider>
+      <RootNavigator />
+    </AuthenticatedUserProvider>
+  )
+}
+
+
 
 
