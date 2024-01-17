570857fd74820982fb8121b783018ee88861d5d8
Christy Segunto <83587757+ChristySegunto@users.noreply.github.com>
2024-01-18 03:38:19 +0800

    ADDED: Add auth to exported modules

--- Firebase/firebase.js
+++ Firebase/firebase.js
@@ -17,6 +17,7 @@ const firebaseConfig ={
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
+const auth = getAuth(app);
 
 
-export { app, db }
+export { app, db, auth }
