64c9e96a7293cd2213a8fa44350ff4f618250240
Christy Segunto <83587757+ChristySegunto@users.noreply.github.com>
2024-01-17 08:13:19 +0800

    ADDED: Stacks for navigation

--- App.js
+++ App.js
@@ -1,20 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import 'react-native-gesture-handler';
+import Login from './Screens/Login';
+import Register from './Screens/Register';
+import Home_Fireauth from './Screens/Home_Fireauth';
+import Home_Resident from './Screens/Home_Resident';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
-export default function App() {
+
+
+const App: () => Node = () => {
+  const Stack = createStackNavigator();
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Login" component={Login} options={{ headerShown: false }} />
+        <Stack.Screen name="Register" component={Register} options={{ headerShown: false }} />
+        <Stack.Screen name="Home_Resident" component={Home_Resident} options={{ headerShown: false }} />  
+        <Stack.Screen name="Home_Fireauth" component={Home_Fireauth} options={{ headerShown: false }} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
+export default App;
+
+
